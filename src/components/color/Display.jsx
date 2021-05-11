16a0a54943c42658044124db1e6e4f015a6db07d
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-05-10 17:48:47 -0700

    repeat image working on page

--- src/components/color/Display.jsx
+++ src/components/color/Display.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = ({ color }) => (
-    <div style={{ width: '50em', height: '50em', background: color }}></div>
+const Display = ({ color, img }) => (
+    <div style={{ width: '50em', height: '50em', backgroundColor: color, backgroundImage: `url(${img})` }}></div>
 );
 
 Display.propTypes = {
--- src/container/RandomColor.jsx
+++ src/container/RandomColor.jsx
@@ -3,14 +3,31 @@ import Display from '../components/color/Display';
 
 export default class RandomColor extends Component {
     state = {
-        color: '#6930C3'
+        color: '#6930C3',
+        img: ''
     };
 
     colors = ['#7400B8', '#6930C3', '#5E60CE', '#5390D9', '#4EA8DE', '#48BFE3', '#56CFE1', '#64DFDF', '#72EFDD', '80FFDB'];
 
+    changeColor = () => {
+        let newColor = this.colors[Math.floor(Math.random() * 10)];
+        if (this.state.color === newColor) {
+            this.setState({ img: 'https://media.wired.com/photos/59094a0ad8c8646f38eef2ae/master/pass/rainbow-sq.jpg' });
+        }
+        else {
+            this.setState({ color: newColor, img: '' });
+        }
+    };
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.changeColor(), 1000);
+    };
+
+    // 
+
     render() {
         return (
-            <Display color={this.state.color} />
+            <Display color={this.state.color} img={this.state.img} />
         );
     }
 }
